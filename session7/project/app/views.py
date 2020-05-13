90986bd8bc26edc3771733a502ed17264ff714d7
dooking <60457112+dooking@users.noreply.github.com>
2020-05-13 21:33:06 +0900

    countword

--- session7/project/app/views.py
+++ session7/project/app/views.py
@@ -12,11 +12,17 @@ def result(request):
     total_len = len(text)
     noblank_text = text.replace(" ","")
     noblank_len = len(noblank_text)
+    cnt = 1
+    for word in text:
+        if(word == ' '):
+            cnt += 1
+
     return render(request, 'result.html', {
         'total_len': total_len,
         'text': text,
         'noblank_text': noblank_text,
         'noblank_len': noblank_len,
+        'word_count' : cnt,
 })
 
 def back(request):
